98e2f7842ceaf806afe3f91e56c9d5255d45d20a
Aakarshan Thapa <akarsan.thapa@gmail.com>
2022-10-19 19:55:45 +0545

    Events page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+// import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+
+// Pages
+import {
+  Home,
+  About,
+  Events,
+  Services,
+  Products,
+  ContactUs,
+  Login,
+  FAQ,
+  Privacy,
+} from "./pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="events" element={<Events />} />
+        <Route path="mail" element={<ContactUs />} />
+        <Route path="products" element={<Products />} />
+        <Route path="services" element={<Services />} />
+        <Route path="login" element={<Login />} />
+        <Route path="faqs" element={<FAQ />} />
+        <Route path="privacy" element={<Privacy />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+
+import { Provider } from "react-redux";
+import store from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+
+// styles
+import "./index.css";
+import "./styles/bootstrap.css";
+import "./styles/font-awesome.css";
+import "./styles/style.css";
+import "./styles/easy-responsive-tabs.css";
+import "./styles/creditly.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
